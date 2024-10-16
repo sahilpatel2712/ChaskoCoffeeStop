cfdd594acb2633877c3f0c53d431ac3e9596024f
Sahil Patel <sahilp14323@gmail.com>
2024-10-16 19:28:41 +0530

    fix image responsive

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -1,23 +1,37 @@
-import React from 'react'
-import page2 from '../resource/page2.jpg'
-import '../App.css'
+import React from "react";
+import page2 from "../resource/page2.jpg";
+import "../App.css";
 const Page2 = () => {
-    return (
-        <>
-            <div className="page2-parent">
-                <div className="page2Data">
-                    <h1 className="page2Heading"><b>Discover the best coffee</b></h1>
-                    <span className="page2contentRestData">Bean Scene is a coffee shop that provides you with quality coffee that helps boost your productivity and helps build your mood. Having a cup of coffee is good, but having a cup of real coffee is greater. There is no doubt that you will enjoy this coffee more than others you have ever tasted.</span>
-                    <div className="LearnMore">
-                        <a className="learnMore" href="#learn">Learn More</a>
-                    </div>
-                </div>
-                <div className="pageImage">
-                    <img src={page2} alt=""></img>
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div className="page2-parent">
+        <div className="page2Data">
+          <h1 className="page2Heading">
+            <b>Discover the best coffee</b>
+          </h1>
+          <span className="page2contentRestData">
+            Bean Scene is a coffee shop that provides you with quality coffee
+            that helps boost your productivity and helps build your mood. Having
+            a cup of coffee is good, but having a cup of real coffee is greater.
+            There is no doubt that you will enjoy this coffee more than others
+            you have ever tasted.
+          </span>
+          <div className="LearnMore">
+            <a className="learnMore" href="#learn">
+              Learn More
+            </a>
+          </div>
+        </div>
+        <div className="pageImage">
+          <img
+            src={page2}
+            alt="cup-img"
+            style={{ height: "100%", width: "100%", objectFit: "contain" }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Page2;
