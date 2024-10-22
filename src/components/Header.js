3331d805193cb134c9f53efb81007acc28714110
Sahil Patel <sahilp14323@gmail.com>
2024-10-22 19:41:15 +0530

    add toggle navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,64 @@
-import React from 'react';
-import '../App.css';
-import logo from '../resource/LatestLogo.png';
+import React from "react";
+import "../App.css";
+import logo from "../resource/LatestLogo.png";
+import { NavbarBurger, NavbarCross } from "../resource/svg/svg";
 
 const Header = () => {
+  const [nav, setNav] = React.useState(false);
   return (
     <header className="header">
       <div className="header-container">
         <div className="logo">
           <img src={logo} alt="Coffee Shop Logo" />
+          <div
+            className="toggle-button"
+            onClick={() => setNav((prev) => !prev)}
+          >
+            {nav ? NavbarCross() : NavbarBurger()}
+          </div>
         </div>
-        <div className="info">
-          <nav className="nav-links">
-            <ul>
-              <li><a href="#home">Home</a></li>
-              <li><a href="#menu">Menu</a></li>
-              <li><a href="#about">About Us</a></li>
-              <li><a href="#contact">Contact</a></li>
-            </ul>
-          </nav>
-        </div>
-        <div className="login">
-          <nav className="nav-links">
-            <ul className="login-links">
-              <li><a href="#signIn">Sign In</a></li>
-              <li className="signup-item"><a href="#signUp">Sign Up</a></li>
-            </ul>
-          </nav>
-        </div>
+        <HeaderItem className={"horizontal"} />
+        {nav ? <HeaderItem className={"vertical"} /> : null}
       </div>
     </header>
   );
 };
 
 export default Header;
+
+const HeaderItem = ({ className }) => {
+  return (
+    <>
+      <div className={"info-" + className}>
+        <nav className="nav-links">
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#menu">Menu</a>
+            </li>
+            <li>
+              <a href="#about">About Us</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className={"login-" + className}>
+        <nav className="nav-links">
+          <ul className="login-links">
+            <li>
+              <a href="#signIn">Sign In</a>
+            </li>
+            <li className="signup-item">
+              <a href="#signUp">Sign Up</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+};
---
+++ src/resource/svg/svg.jsx
@@ -0,0 +1,37 @@
+export const NavbarBurger = () => {
+  return (
+    <>
+      <svg
+        width="25px"
+        height="25px"
+        viewBox="0 0 1024 1024"
+        className="icon"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M64 192h896v76.8H64V192z m0 281.6h896v76.8H64V473.6z m0 281.6h896V832H64v-76.8z"
+          fill="currentColor"
+        />
+      </svg>
+    </>
+  );
+};
+
+export const NavbarCross = () => {
+  return (
+    <svg
+      width="25px"
+      height="25px"
+      viewBox="0 0 1024 1024"
+      className="icon"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M512 457.6L905.6 64l54.336 54.336-393.6 393.6L960 905.6l-54.4 54.4L512 566.336 118.4 960 64 905.6 457.6 512 64 118.336 118.336 64l393.6 393.6z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+};
